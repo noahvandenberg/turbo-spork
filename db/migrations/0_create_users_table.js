bbeb9ce9ba630a91fdf21bda6fcc9fd09904341b
Noah V <noah@noahvandenberg.com>
2022-03-18 12:51:18 -0700

    update create user table migration to reflect project erd

--- db/migrations/0_create_users_table.js
+++ db/migrations/0_create_users_table.js
@@ -11,8 +11,24 @@
     table
       .string("email")
       .notNullable();
-    table.string('name').notNullable();
-    table.string('password').notNullable();
+    table
+      .string('name')
+      .notNullable();
+    table
+      .string('password')
+      .notNullable();
+    table
+      .string('bio');
+    table
+      .string('profile_image_url');
+    table
+      .string('gender');
+    table
+      .date('birth_date');
+    table
+      .string('sports');
+    table
+      .integer('km_range')
   });
 };
 
