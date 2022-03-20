b1961372369514380954e6167c2291b60e665214
Noah V <noah@noahvandenberg.com>
2022-03-20 13:03:00 -0700

    add try/catch to routes

--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,47 @@ var router = express.Router();
 
 // BROWSE GET
 router.get('/', async(req, res) => {
-  res.send('respond with a resource');
+  try {
+    res.send('respond with a resource');
+  } catch (error) {
+
+  }
 });
 
 // READ GET
 router.get('/:id', async(req, res) => {
-  res.send('respond with a resource');
+  try {
+    res.send('respond with a resource');
+  } catch (error) {
+
+  }
 });
 
 // EDIT PUT
 router.get('/:id', async(req, res) => {
-  res.send('respond with a resource');
+  try {
+    res.send('respond with a resource');
+  } catch (error) {
+
+  }
 });
 
 // ADD POST
 router.get('/', async(req, res) => {
-  res.send('respond with a resource');
+  try {
+    res.send('respond with a resource');
+  } catch (error) {
+
+  }
 });
 
 // DELETE DELETE
 router.delete('/:id', async(req, res) => {
-  res.send('respond with a resource');
+  try {
+    res.send('respond with a resource');
+  } catch (error) {
+
+  }
 });
 
 module.exports = router;
