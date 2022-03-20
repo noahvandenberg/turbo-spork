dbaec364d5e9e0b3f06858d5b38441c9dc344f78
Noah V <noah@noahvandenberg.com>
2022-03-20 13:52:39 -0700

    add database queries to event routes

--- routes/events.js
+++ routes/events.js
@@ -6,7 +6,10 @@ var router = express.Router();
 // BROWSE GET
 router.get('/', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'SELECT * FROM events;',
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -15,7 +18,11 @@ router.get('/', async(req, res) => {
 // READ GET
 router.get('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'SELECT * FROM events WHERE id = $1;',
+      values: [req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -24,7 +31,14 @@ router.get('/:id', async(req, res) => {
 // EDIT PUT
 router.get('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    // req.body object destructuring
+    const [user_owner, datetime, title, description, location, sport, same_gender, similar_age] = req.body
+
+    const query = {
+      text: 'UPDATE events SET user_owner=$1, datetime=$2, title=$3, description=$4, location=$5, sport=$6, same_gender=$7, similar_age=$8 WHERE id=$9 RETURNING *;',
+      values: [user_owner, datetime, title, description, location, sport, same_gender, similar_age, req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -33,7 +47,14 @@ router.get('/:id', async(req, res) => {
 // ADD POST
 router.get('/', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    // req.body object destructuring
+    const [user_owner, datetime, title, description, location, sport, same_gender, similar_age] = req.body
+
+    const query = {
+      text: 'INSERT INTO events (user_owner, datetime, title, description, location, sport, same_gender, similar_age) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;',
+      values: [user_owner, datetime, title, description, location, sport, same_gender, similar_age],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -42,7 +63,11 @@ router.get('/', async(req, res) => {
 // DELETE DELETE
 router.delete('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'DELETE FROM events WHERE id = $1 RETURNING *;',
+      values: [req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
