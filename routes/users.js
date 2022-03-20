ee6c3ca3de5f2afc400c61f8382c6ef25826a4cd
Noah V <noah@noahvandenberg.com>
2022-03-20 13:49:18 -0700

    add database queries to user routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const req = require('express/lib/request');
 var router = express.Router();
 
 // BREAD ROUTES
@@ -6,7 +7,10 @@ var router = express.Router();
 // BROWSE GET
 router.get('/', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'SELECT * FROM users;',
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -15,7 +19,11 @@ router.get('/', async(req, res) => {
 // READ GET
 router.get('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'SELECT * FROM users WHERE id = $1;',
+      values: [req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -24,7 +32,14 @@ router.get('/:id', async(req, res) => {
 // EDIT PUT
 router.get('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    // req.body object destructuring
+    const [email, name, password, bio, profile_image_url, gender, birthdate, sports, km_range] = req.body
+
+    const query = {
+      text: 'UPDATE users SET email=$1, name=$2, password=$3, bio=$4, profile_image_url=$5, gender=$6, birthdate=$7, sports=$8, km_range=$9 WHERE id=$10 RETURNING *;',
+      values: [email, name, password, bio, profile_image_url, gender, birthdate, sports, km_range, req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -33,7 +48,14 @@ router.get('/:id', async(req, res) => {
 // ADD POST
 router.get('/', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    // req.body object destructuring
+    const [email, name, password, bio, profile_image_url, gender, birthdate, sports, km_range] = req.body
+
+    const query = {
+      text: 'INSERT INTO users (email, name, password, bio, profile_image_url, gender, birthdate, sports, km_range) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;',
+      values: [email, name, password, bio, profile_image_url, gender, birthdate, sports, km_range],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
@@ -42,7 +64,11 @@ router.get('/', async(req, res) => {
 // DELETE DELETE
 router.delete('/:id', async(req, res) => {
   try {
-    res.send('respond with a resource');
+    const query = {
+      text: 'DELETE FROM users WHERE id = $1 RETURNING *;',
+      values: [req.params.id],
+    };
+    const dbResponse = await db.query(query);
   } catch (error) {
 
   }
